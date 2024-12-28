edd4e9646da10e24c04c61673fcf02ea36b6960c
parallels <hudsonhilal@gmail.com>
2024-12-28 20:11:28 +0100

    changed transformer stuff#

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from datetime import timedelta
-
+from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
 
 @app.route("/chat", methods=["POST", "GET"])
 def chat():
     recent_chat = " "
     if request.method == "POST":
         chat = request.form["chat"]
-        recent_chat = chat
+        user_input = tokenizer.encode(chat, return_tensors="pt")
+        output = model.generate(user_input, max_length=50)
+        recent_chat = tokenizer.decode(output[0], skip_special_tokens=True)
     return render_template("chat.html", chat=recent_chat)
 
 
